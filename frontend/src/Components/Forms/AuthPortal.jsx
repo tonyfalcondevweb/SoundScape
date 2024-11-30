572f991780e5c3fa392a218cee522e4c37121a34
tonyfalcondevweb <tonyfalcondevweb@gmail.com>
2024-11-30 21:52:49 +0100

    fix: css tailwind responsive

--- frontend/src/Components/Forms/AuthPortal.jsx
+++ frontend/src/Components/Forms/AuthPortal.jsx
@@ -13,10 +13,10 @@ const AuthPortal = () => {
 
   return (
     <Container className="flex justify-center items-center">
-      <div className="flex flex-row space-x-20 items-center justify-center">
+      <div className="flex lg:flex-row flex-col lg:space-x-20 space-y-20 items-center justify-center">
         <Login setLoad={setLoad} />
 
-        <div className="h-60 w-2 bg-slate-800 mx-4 rounded-full"></div>
+        <div className="lg:h-60 lg:w-2 h-2 w-60 bg-slate-800 mx-4 rounded-full"></div>
 
         <Register setLoad={setLoad} />
       </div>
